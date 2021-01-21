8e6b7daa000aa444f3c6b17269ab7aa1ce600f37
David <davidelbar2@gmail.com>
2021-01-21 14:05:45 +0100

    ok

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-//import Map from "./Map/Map";
+import Map from "./Map/Map";
 
 function App() {
   return (
     // Important! Always set the container height explicitly
     <div className="App">
-      hey
+      <Map />
     </div>
   );
 }
