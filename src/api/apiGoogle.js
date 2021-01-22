d39d5e46ed0dbf7fec8378639bb0c68933227f19
David <davidelbar2@gmail.com>
2021-01-22 10:10:40 +0100

    test

--- src/api/apiGoogle.js
+++ src/api/apiGoogle.js
@@ -1,3 +1,3 @@
-//export const googleKey = ''
+export const googleKey = 'AIzaSyB1HpSbn4bSaD72hicFEnAezu8JItE9LvY'
 //AIzaSyCnLKbU8bgG8LHDV_wZ7R4xBws_pGBp1ww
 //https://console.cloud.google.com/billing/011937-AD6167-A89CDF/manage?hl=fr&project=sncf-293114
